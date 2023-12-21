11580e2a22ef20d49d1c43e38e7cdd79687681e8
VanessaYezerska <126601052+VanessaYezerska@users.noreply.github.com>
2023-12-21 17:57:53 +0200

    Update WaiterService.java

--- Rest/WaiterService.java
+++ Rest/WaiterService.java
@@ -1,10 +1,8 @@
 package Rest;
 
  interface WaiterService {
-     // Абстрактний метод для обслуговування столів
+  
      void serveTable(int tableNumber);
-
-     // Абстрактний метод для вітання клієнта
      void greetCustomer();
  }
 
