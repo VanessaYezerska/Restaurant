2c7d201ba9b70a64e75586e7c35fe7c8000bdfac
VanessaYezerska <126601052+VanessaYezerska@users.noreply.github.com>
2023-12-21 17:57:03 +0200

    Update Waiter.java

--- Rest/Waiter.java
+++ Rest/Waiter.java
@@ -1,27 +1,22 @@
 package Rest;
 
  class Waiter implements WaiterService {
-     // Змінні класу Waiter
      String name;
 
-     // Конструктор класу WaiterOne
      public Waiter(String name) {
          this.name = name;
      }
 
-     // Реалізація методу інтерфейсу для обслуговування столів
      @Override
      public void serveTable(int tableNumber) {
          System.out.println("WaiterOne " + name + " is serving table " + tableNumber);
      }
 
-     // Реалізація методу інтерфейсу для вітання клієнта
      @Override
      public void greetCustomer() {
          System.out.println("WaiterOne " + name + " is greeting the customer");
      }
 
-     // Перевантажений метод для класу WaiterOne
      public void additionalMethod() {
          System.out.println("WaiterOne " + name + " has an additional method");
      }
